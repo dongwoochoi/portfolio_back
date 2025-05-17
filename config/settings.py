bf2badbf5575597f7a563391d64274e085aa52d3
dong99woo <dong99woo@gmail.com>
2025-05-17 18:37:39 +0900

    Refactor: 페이지네이션 세팅 설정

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 9,  # 한 페이지당 게시글 수
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
